a59561eb2a5ce865cc8e1023a7d7de37f017f456
RubenTeles <ruben-teles@hotmail.com>
2023-11-27 11:53:27 +0000

    ex01 and ex02

--- 00/ex01/format_ft_time.py
+++ 00/ex01/format_ft_time.py
@@ -2,15 +2,31 @@
 import time
 from datetime import datetime
 
-# Obter o tempo atual em segundos desde 01/01/1970 (época)
-current_time_seconds = time.time()
 
-# Formatar o tempo em notação científica
-scientific_notation = "{:.4e}".format(current_time_seconds)
 
-# Obter a data atual
-current_date = datetime.now().strftime("%b %d %Y")
+#Seconds since January 1, 1970: 1,666,355,857.3622 or 1.67e+09 in scientific notation$
 
-# Imprimir os resultados
-print(f"Seconds since January 1, 1970: {current_time_seconds:.4f} or {scientific_notation} in scientific notation$")
-print(current_date)
+first_date = datetime(1970, 1, 1)
+current_date = datetime.now()
+
+seconds = (current_date - first_date).total_seconds()
+scientific_notation = "{:e}".format(seconds)
+
+#NOV 27 2023
+format_date = "%b %d %Y" 
+
+print("Seconds since January 1, 1970: ", seconds, " or ", scientific_notation, " in scientific notation")
+print(current_date.strftime(format_date))
+
+# %Y: Ano com quatro dígitos (ex: 2023).
+# %m: Mês como número decimal com zero à esquerda (01 a 12).
+# %d: Dia do mês como número decimal com zero à esquerda (01 a 31).
+# %H: Hora (00 a 23).
+# %M: Minutos (00 a 59).
+# %S: Segundos (00 a 59).
+# %a: Abreviação do nome do dia da semana (Sun, Mon, Tue, etc.).
+# %A: Nome completo do dia da semana (Sunday, Monday, Tuesday, etc.).
+# %b: Abreviação do nome do mês (Jan, Feb, Mar, etc.).
+# %B: Nome completo do mês (January, February, March, etc.).
+
+# time.time() daria o tempo em segundos desde 1/1/1970
