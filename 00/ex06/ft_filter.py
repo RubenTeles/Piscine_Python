3d1f5a3363e2d7d3eababf8f33368f91f7952fb6
Ruben Teles <rubenteles@hotmail.com>
2025-09-20 22:59:58 +0100

    ex06 otimization

--- 00/ex06/ft_filter.py
+++ 00/ex06/ft_filter.py
@@ -1,12 +1,10 @@
 def ft_filter(function, iterable):
-    """filter(function or None, iterable) --> filter object
-
+    """filter(function or None, iterable) --> filter object\n
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true."""
     if function is None:
         return (item for item in iterable if item)
-    else:
-        return (item for item in iterable if function(item))
+    return (item for item in iterable if function(item))
     
 ## List-Comprehension 
 #https://www.datacamp.com/tutorial/python-list-comprehension?utm_aid=157156375191&utm_loc=9208947-&utm_mtd=-c&utm_kw=&gad_campaignid=19589720821
